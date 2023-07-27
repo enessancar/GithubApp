c60ee7b5b94f59775487357ce763a1bf498b8316
enessancar <enes57751907@gmail.com>
2023-07-27 21:49:21 +0300

    Initial Commit

---
+++ GithubApp/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  GithubApp
+//
+//  Created by Enes Sancar on 27.07.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+       
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
