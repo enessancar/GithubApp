2d1f966d233b184c4db29e733eb9c5f779b9b092
enessancar <enes57751907@gmail.com>
2023-08-05 13:54:01 +0300

    I wrote custom image component for in-app use

---
+++ GithubFollowers/CustomViews/ImageViews/GFAvatarImageView.swift
@@ -0,0 +1,38 @@
+//
+//  GFAvatarImageView.swift
+//  GithubFollowers
+//
+//  Created by Enes Sancar on 5.08.2023.
+//
+
+import UIKit
+
+final class GFAvatarImageView: UIImageView {
+    
+    let cache = NetworkManager.shared.cache
+    let placeHolderImage = Images.placeholder
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeHolderImage
+    }
+    
+    private func downloadImage(urlString: String) {
+        NetworkManager.shared.downloadImage(urlString: urlString) { [weak self] image in
+            guard let self else { return }
+            DispatchQueue.main.async {
+                self.image = image
+            }
+        }
+    }
+}
