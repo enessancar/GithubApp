2f6634b83aeddbc7a56b707476b45e8172bb7bc1
enessancar <enes57751907@gmail.com>
2023-08-06 23:31:13 +0300

    I created view controller ext for safari orientation and alertVC

--- GithubFollowers/Extensions/UIViewController+Ext.swift
+++ GithubFollowers/Extensions/UIViewController+Ext.swift
@@ -6,7 +6,22 @@
 //
 
 import UIKit
+import SafariServices
 
 extension UIViewController {
     
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+    
+    func presentSafariVC(url: URL) {
+        let safariVC = SFSafariViewController(url: url)
+        safariVC.preferredControlTintColor = .systemGreen
+        present(safariVC, animated: true)
+    }
 }
--- GithubFollowers/Screens/SearchVC.swift
+++ GithubFollowers/Screens/SearchVC.swift
@@ -6,10 +6,85 @@
 //
 
 import UIKit
+import SnapKit
 
 final class SearchVC: UIViewController {
     
+    //MARK: - Properties
+    private let logoImageView = UIImageView()
+    private let usernameTextField = GFTextField()
+    private let actionButton = GFButton(title: "Get Followers", backgroundColor: .systemGreen)
+    
+    var isUsernameEntered: Bool {
+        !usernameTextField.text!.isEmpty
+    }
+    
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        view.addSubviews(logoImageView, usernameTextField, actionButton)
+        configureLogoImageView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        usernameTextField.text = ""
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    private func createDismissKeyboard() {
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(dismissVC))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc private func dismissVC() {
+        view.endEditing(true)
+    }
+    
+    @objc private func pushFollowerListVC() {
+        guard isUsernameEntered else {
+            
+        }
+    }
+    
+    private func configureLogoImageView() {
+        logoImageView.image = Images.ghLogo
+        
+        logoImageView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(80)
+            make.centerX.equalToSuperview()
+            make.width.height.equalTo(200)
+        }
+    }
+    
+    private func configureTextField() {
+        usernameTextField.delegate = self
+        
+        usernameTextField.snp.makeConstraints { make in
+            make.top.equalTo(logoImageView.snp.bottom).offset(48)
+            make.leading.equalToSuperview().offset(50)
+            make.trailing.equalToSuperview().offset(-50)
+            make.height.equalTo(50)
+        }
+    }
+    
+    private func configureActionButton() {
+        actionButton.addTarget(self, action: #selector(pushFollowerListVC), for: .touchUpInside)
+        
+        actionButton.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-50)
+            make.leading.trailing.height.equalTo(usernameTextField)
+        }
+    }
+}
+
+
+extension SearchVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        pushFollowerListVC()
+        return true
     }
 }
