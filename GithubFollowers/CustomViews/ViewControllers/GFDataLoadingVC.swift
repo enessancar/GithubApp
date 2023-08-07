b1273e2c7ed4d10b9463d385395500b7ae5b9748
enessancar <enes57751907@gmail.com>
2023-08-07 11:17:56 +0300

    I created data loading VC to show to the user

--- GithubFollowers/CustomViews/ViewControllers/GFDataLoadingVC.swift
+++ GithubFollowers/CustomViews/ViewControllers/GFDataLoadingVC.swift
@@ -5,4 +5,44 @@
 //  Created by Enes Sancar on 6.08.2023.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class GFDataLoadingVC: UIViewController {
+    
+    private var containerView: UIView!
+    
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) {
+            self.containerView.alpha = 0.8
+        }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        activityIndicator.startAnimating()
+    }
+    
+    func dismissLoadingVC() {
+        DispatchQueue.main.async {
+            self.containerView.removeFromSuperview()
+            self.containerView = nil
+        }
+    }
+    
+    func showEmptyStateView(message: String, view: UIView) {
+        let emptyStateView = GFEmptyStateView(message: message)
+        emptyStateView.frame = view.frame  
+        view.addSubview(emptyStateView)
+    }
+}
